b7ecbe909f6fe535176dfc42d26ec2cb58ced021
Jian Yu <mgcnrx11@hotmail.com>
2013-08-04 23:43:51 +0800

    完善导入，增加格式检查，修复多个导入bug

--- src/main/java/im/jianyu/los_dlc_import/App.java
+++ src/main/java/im/jianyu/los_dlc_import/App.java
@@ -10,68 +10,160 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 /**
  * Hello world!
  * 
  */
 public class App {
-	
+
 	public List<String> tag;
-	
+
 	public Map<String, String> enMap;
-	
+
 	public Map<String, String> cnMap;
-	
+
 	int i = 0;
-	
+
 	public static void main(String[] args) {
 		App app = new App();
-		app.ReadDataReturn();
-		
+		//app.ReadDataReturn();
+		app.ReadData();
+		System.out.println("tag size: "+app.tag.size());
+		System.out.println("cn size: "+app.cnMap.size());
+
 		app.importData();
-		
-		for (String str:app.cnMap.keySet()) {
-			System.out.println(app.cnMap.get(str));
+
+		System.out.println("没有被替换的：");
+		for (String str : app.cnMap.keySet()) {
 			System.out.println(app.enMap.get(str));
+			System.out.println(app.cnMap.get(str));
 		}
-		
+
 		System.out.println("一共替换了" + app.i + "个");
+		
+		app.checkEng();
 	}
 	
+	public void checkImportFile() {
+		try {
+			FileReader reader = new FileReader(
+					"/Volumes/Macintosh Data/Yujian/Dropbox/los/test");
+			BufferedReader br = new BufferedReader(reader);
+			String row;
+			while ((row = br.readLine()) != null) {
+				if(row.startsWith("####")) {
+					br.readLine();
+					if (br.readLine().equals("")) {
+					} else {
+						System.out.println(row);
+					}
+				} else {
+					System.out.println(row);
+				}
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void checkEng() {
+		try {
+			FileReader reader = new FileReader(
+					"/Volumes/Macintosh Data/Yujian/Dropbox/los/test.txt");
+			BufferedReader br = new BufferedReader(reader);
+			String row;
+			String engScript="";
+			
+			FileWriter fileWriter = new FileWriter(
+					"/Volumes/Macintosh Data/Yujian/Dropbox/los/unrelaced" + ".txt");
+
+			int rowCount = 0;
+			int engCount = 0;
+			int chnCount = 0;
+			int scriptCount = 0;
+			char[] wlnChar = { '\r', '\n' };
+			while ((row = br.readLine()) != null) {
+				rowCount++;
+				boolean containChinese = false;
+
+				if (row.startsWith("#")) {
+					engScript = br.readLine();
+					scriptCount++;
+					rowCount++;
+					for (int i = 0; i < engScript.length(); i++) {
+						if(engScript.substring(i, i + 1).matches(
+								"[\\u4e00-\\u9fa5]+")) {
+							containChinese = true;
+							chnCount++;
+							break;
+						}
+					}
+				}
+				
+				if (containChinese == false && !row.equals("")) {
+					//System.out.println("Row " + rowCount + " is Eng!");
+					if (row.startsWith("#### CREDIT")) {
+						//break;
+					}
+					engCount++;
+					fileWriter.write(row);
+					fileWriter.write(wlnChar);
+					fileWriter.write(engScript);
+					fileWriter.write(wlnChar);
+					fileWriter.write(wlnChar);
+				}
+			}
+			
+			fileWriter.close();
+			br.close();
+			
+			System.out.println("Total " + engCount + " unreplaced");
+			System.out.println("Total " + chnCount + " replaced");
+			System.out.println("Total " + scriptCount + " script");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void importData() {
 		try {
-			FileReader read = new FileReader("/Volumes/Macintosh Data/Yujian/Dropbox/los/test");
-			FileWriter fileWriter = new FileWriter("/Volumes/Macintosh Data/Yujian/Dropbox/los/test" + ".txt");  
+			FileReader read = new FileReader(
+					"/Volumes/Macintosh Data/Yujian/Dropbox/los/test");
+			FileWriter fileWriter = new FileWriter(
+					"/Volumes/Macintosh Data/Yujian/Dropbox/los/test" + ".txt");
 			BufferedReader br = new BufferedReader(read);
 			String row;
 			char[] wlnChar = { '\r', '\n' };
-			
-			while((row = br.readLine()) != null) {
+
+			while ((row = br.readLine()) != null) {
 				boolean isMatch = false;
+				
 
 				for (String str : tag) {
 					String trimRow;
 					trimRow = row.replace("#### ", "");
 					trimRow = trimRow.replace(" ####", "");
-					if (trimRow.equals(str)) {
-						//System.out.println(row + "--" + i);
+					if (trimRow.equals(str) && !trimRow.equals(row)) {
 						fileWriter.write(row);
 						fileWriter.write(wlnChar);
-						row = br.readLine();
-						if (row.contains(enMap.get(str))) {
-							//System.out.println(cnMap.get(str) + "--" + i++);
-							
+						br.readLine();
+						try {
 							fileWriter.write(cnMap.get(str));
 							fileWriter.write(wlnChar);
 							cnMap.remove(str);
 							isMatch = true;
 							i++;
-							continue;
+							break;
+						} catch (NullPointerException e) {
+							System.out.println(i+"--"+row);
 						}
 					}
 				}
-				if (isMatch) continue;
+
+				if (isMatch)
+					continue;
 				fileWriter.write(row);
 				fileWriter.write(wlnChar);
 			}
@@ -84,43 +176,49 @@ public class App {
 
 	public void ReadData() {
 		try {
-			FileReader read = new FileReader("/Volumes/Macintosh Data/Yujian/Dropbox/los/LoS翻译成品0729/LoS原画标题_ch_完成_130729.txt");
+			FileReader read = new FileReader(
+					"/Volumes/Macintosh Data/Yujian/Dropbox/los/LoS翻译成品0729/LoS原画标题_ch_完成_130729.txt");
 			BufferedReader br = new BufferedReader(read);
 			String row;
 			tag = new ArrayList<String>();
 			enMap = new HashMap<String, String>();
 			cnMap = new HashMap<String, String>();
 			while ((row = br.readLine()) != null) {
-				if (row.equals("")) continue;
+				if (row.equals(""))
+					continue;
 				String[] strArray = row.split("	");
 				tag.add(strArray[0]);
 				enMap.put(strArray[0], strArray[1]);
 				cnMap.put(strArray[0], strArray[2]);
-				//System.out.println(row);
+				// System.out.println(row);
 			}
+			br.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void ReadDataReturn() {
 		try {
-			FileReader read = new FileReader("/Volumes/Macintosh Data/Yujian/Dropbox/los/LoS翻译成品0729/LoS其他提示_ch_完成_130731.txt");
+			FileReader read = new FileReader(
+					"/Volumes/Macintosh Data/Yujian/Dropbox/los/LoS翻译成品0729/LoS其他提示_ch_完成_130731改.txt");
 			BufferedReader br = new BufferedReader(read);
 			String row;
 			tag = new ArrayList<String>();
 			enMap = new HashMap<String, String>();
 			cnMap = new HashMap<String, String>();
 			while ((row = br.readLine()) != null) {
-				if (row.equals("")) continue;
+				if (row.equals(""))
+					continue;
 				String[] strArray = row.split("	");
 				tag.add(strArray[0]);
 				enMap.put(strArray[0], strArray[1]);
 				cnMap.put(strArray[0], br.readLine());
-				//System.out.println(row);
+				// System.out.println(row);
 			}
+			br.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
--- src/test/java/im/jianyu/los_dlc_import/AppTest.java
+++ src/test/java/im/jianyu/los_dlc_import/AppTest.java
@@ -34,5 +34,7 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+        App app = new App();
+        app.checkImportFile();
     }
 }
